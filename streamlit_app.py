9bd98ddb33af3e8d8b9edc9884657892966d5a9a
Alex-101-s <115874239+Alex-101-s@users.noreply.github.com>
2023-07-18 22:29:40 +0530

    Update streamlit_app.py
    added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit  # any small spelling mistake it won't relflect on streamlit
-streamlit.title('My parents New healthy dinner')
-streamlit.header('breakfast menu')
-streamlit.text('omega3 and Blueberry oatmeals')
-streamlit.text('kale, spinach and Rocket')
+streamlit.title('My parents New healthy dinner')   
+streamlit.header('🥣 breakfast menu')
+streamlit.text('🥗 omega3 and Blueberry oatmeals')
+streamlit.text('🐔🥑🍞kale, spinach and Rocket')
