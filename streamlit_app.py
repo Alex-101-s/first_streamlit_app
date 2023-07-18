d5e68e5919139131b883b1d95c5a281ad00e6d5f
Alex-101-s <115874239+Alex-101-s@users.noreply.github.com>
2023-07-18 22:11:51 +0530

    Update streamlit_app.py
    It's header button

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,3 @@
 import streamlit  # any small spelling mistake it won't relflect on streamlit
 streamlit.title('My parents New healthy dinner')
-streamlit.title('omega3 and blueberry oatmeals')
+streamlit.header('breakfast menu')
