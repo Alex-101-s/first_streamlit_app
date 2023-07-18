b6865b2d50361218e6f7543e7dd3bcba34874305
Alex-101-s <115874239+Alex-101-s@users.noreply.github.com>
2023-07-18 22:10:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,3 @@
 import streamlit  # any small spelling mistake it won't relflect on streamlit
 streamlit.title('My parents New healthy dinner')
-streamlit.header('omega3 and blueberry oatmeals')
+streamlit.title('omega3 and blueberry oatmeals')
