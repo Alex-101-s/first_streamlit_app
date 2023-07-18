95c1ee2e76b6539a4f076fd9a76aea9ff42c1e51
Alex-101-s <115874239+Alex-101-s@users.noreply.github.com>
2023-07-18 22:14:33 +0530

    Update streamlit_app.py
    added text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
 import streamlit  # any small spelling mistake it won't relflect on streamlit
 streamlit.title('My parents New healthy dinner')
 streamlit.header('breakfast menu')
+streamlit.text('omega3 and Blueberry oatmeals')
+streamlit.text('kale, spinach and Rocket')
