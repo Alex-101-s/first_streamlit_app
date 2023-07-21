484a634de1aac01533bb517698107728829a0eee
Alex-101-s <115874239+Alex-101-s@users.noreply.github.com>
2023-07-21 19:49:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,3 +22,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #streamlit.dataframe(my_fruit_list)  #here because above the table we want select option before we did this
 
 streamlit.dataframe(fruits_to_show)
+
+
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
