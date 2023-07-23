a779a5c3c84fa220804786b743bf956452fc17b3
Alex-101-s <115874239+Alex-101-s@users.noreply.github.com>
2023-07-23 22:50:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ def get_fruityvice_data(this_fruit_choice):
 streamlit.header("Fruityvice Fruit Advice!")
 try:
  # Add a Text Entry Box and Send the Input to Fruityvice as Part of the API Call
-    fruit_choice = streamlit.text_input('What fruit would you like information about?','watermelon')
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
     if not fruit_choice:
         streamlit.error("please select a fruit to get information")
     else:
